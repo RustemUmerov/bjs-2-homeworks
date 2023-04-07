20967e9afaa55722ffb75794772f87a90fe9c63a
Rustem Umerov <rustem.amdy.umerov@gmail.com>
2023-04-07 03:10:09 +0300

    подправим снова

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,50 +5,55 @@ class AlarmClock {
   }
 
   addClock(time, callback, id) {
-    if (!id) {
+    if (!time || !callback) {
       throw new Error('Отсутствуют обязательные аргументы');
     }
 
-    if (this.alarmCollection.find(alarm => alarm.id === id)) {
+    if (this.alarmCollection.some(alarm => alarm.time === time)) {
       console.warn('Уже присутствует звонок на это же время');
       return;
     }
 
-    this.alarmCollection.push({ id, time, callback, canCall: true });
+    this.alarmCollection.push({
+      id: id,
+      time: time,
+      callback: callback,
+      canCall: true,
+    });
   }
 
   removeClock(id) {
-    const alarmIndex = this.alarmCollection.findIndex(alarm => alarm.id === id);
-
-    if (alarmIndex === -1) {
-      return false;
+    const index = this.alarmCollection.findIndex(alarm => alarm.id === id);
+    if (index !== -1) {
+      this.alarmCollection.splice(index, 1);
     }
-
-    this.alarmCollection.splice(alarmIndex, 1);
-    return true;
   }
 
   getCurrentFormattedTime() {
     const now = new Date();
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
-
     return `${hours}:${minutes}`;
   }
 
   start() {
-    if (this.intervalId) {
+    if (this.intervalId !== null) {
       return;
     }
 
-    this.intervalId = setInterval(() => {
+    const checkAlarms = () => {
+      const currentTime = this.getCurrentFormattedTime();
+
       this.alarmCollection.forEach(alarm => {
-        if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+        if (alarm.time === currentTime && alarm.canCall) {
           alarm.canCall = false;
           alarm.callback();
         }
       });
-    }, 1000);
+    };
+
+    checkAlarms();
+    this.intervalId = setInterval(checkAlarms, 1000);
   }
 
   stop() {
@@ -57,7 +62,9 @@ class AlarmClock {
   }
 
   resetAllCalls() {
-    this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    this.alarmCollection.forEach(alarm => {
+      alarm.canCall = true;
+    });
   }
 
   clearAlarms() {
